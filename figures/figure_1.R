bd444abb9ec626aea2bab458b6d90afc6cdc7d23
sarika1999 <sarika1999@gmail.com>
2024-08-04 16:47:01 -0400

    update to figure code

--- figures/figure_1.R
+++ figures/figure_1.R
@@ -1,4 +1,4 @@
-#FIGURE 1, panel A: plot of number of floods in a state over 2000-2016 (does remove zipcodes or floods that we exclude) - 6/23/2024
+#FIGURE 1: plot of number of flood events in a ZIP Code 2000-2016 (does remove ZIP Codes or floods that we exclude) 
 library(dplyr)
 library(stringr)
 
@@ -7,14 +7,10 @@ library(sf)
 library(tmap)
 library(usmap)
 
-
 library(RColorBrewer)
-library(ggplot2)
-library(ggthemes)
-library(ggpubr)
 
 output.folder <- #output directory 
-
+  
 load('zipcode_flood_subset_2000_2016_APR.Rdata') #load data 
 flood_data_test$floodzip_id <- paste0(flood_data_test$flood_id, "_flood_df_", flood_data_test$zip)
 
@@ -25,62 +21,43 @@ load("AK_zips.Rdata") #load Alaska zipcodes
 flood_data_test<- flood_data_test[!(flood_data_test$floodzip_id %in% c(floodzips_remove)),] 
 flood_data_test <- flood_data_test[!(flood_data_test$zip %in% c(missing_zipcodes, AK_zips)),]
 
-tab_by_state <- as.data.frame(table(flood_data_test$state_abbrev, flood_data_test$flood_id))
-tab_by_state$indicator <- ifelse(tab_by_state$Freq > 0, 1, 0)
-final_count <- tab_by_state %>% group_by(Var1) %>% summarise(sum(indicator))
+number_flooded <- as.data.frame(table(flood_data_test$zip))
 
-dat <- usmap::us_map(regions = c("states"))
-state_mappy <- left_join(dat, final_count, by = c("abbr" = "Var1"))
-state_mappy[is.na(state_mappy)] <- 0
+import_zipcodeSHP <- function(path, years){
+  zipcode_shp_files <- dir(path, full.names = T, recursive = T)
+  
+  zipcode_shp_files <- zipcode_shp_files[grepl('shp$', zipcode_shp_files)]
+  zipcode_shp_files <- zipcode_shp_files[grepl('polygon', zipcode_shp_files)]
+  
+  zipcode_polygons <- lapply(zipcode_shp_files, st_read)
+  zipcode_polygons <- setNames(zipcode_polygons, years)
+  
+  return(zipcode_polygons)
+}
+
+year <- c("") #pick a year to use for the shapefile 
+
+zip_path <- #path to zipcode shapefiles 
+
+zipcode_polygons <- import_zipcodeSHP(zip_path, years)
+
+zip <- zipcode_polygons[[""]] #fill in with year chosen above 
+merged <- left_join(zip, numb_flooded, by = c("ZIP" = "Var1"))
+merged_2010[is.na(merged)] <- 0
+
+#check how many zipcodes are missing from the shapefile for the particular year 
+numb_flooded$missingness <- ifelse(numb_flooded$Var1 %in% c(zip$ZIP), 1, 0)
+sum(numb_flooded$missingness == 0) 
+
+#exclude zipcodes in Alaska, Hawaii, and Puerto Rico
+contig_us <- merged[!(merged$STATE %in% c("AK", "HI", "PR")),]
 
 #change color palette 
 colorpalette <- c('cornsilk',"lightblue", "blue","darkblue","black")
 sf_use_s2(FALSE)
-names(colorpalette) <- seq(0,40,10)
 
-state_mappy2 <- state_mappy %>% filter(!(abbr %in% c("HI", "AK")))
-state_map_flood <- ggplot() + geom_polygon(data = state_mappy2, 
-                                           aes(x = x, y = y, group = group, fill = `sum(indicator)`), 
-                                           color = "black", size = 0.2) + scale_fill_gradientn(name="Number of floods", colors = colorpalette,
-                                                                                               limits = c(0,40), breaks=c(5,10,15,20,25,30,35)) +
-  theme(legend.position = "bottom",
-        legend.box.spacing = unit(0, "pt"),
-        legend.margin=margin(0,0,0,0)) +
-  ggtitle("") + xlim("") + ylim("") +
-  xlab("") + ylab("")
-
-
-#FIGURE 1, panel B: plot of exposed population 
-
-# load ccs lookup
-code.lookup.merged = read.csv('CCS_DX.csv')
-code.lookup.merged = subset(code.lookup.merged, !(code_chapter%in%c("Complications of pregnancy; childbirth; and the puerperium", 
-                                                                    "Congenital anomalies",
-                                                                    "Certain conditions originating in the perinatal period",
-                                                                    "Symptoms; signs; and ill-defined conditions and factors influencing health status",
-                                                                    "Residual codes; unclassified; all E codes [259. and 260.]")))
-
-# make list of broad causes of hospitalization (level 1 names)
-causes_groups = unique(as.character(code.lookup.merged$code_chapter))
-
-years <- c(2000:2016)
-
-dir.input = # directory to load data from
-
-for (i in 1:length(causes_groups)){
-  input.file = # CCS level 1 input file
-  dat <- readRDS(paste0(input.file))
-  dat <- dat[!(dat$floodzip_id %in% c(floodzips_remove)),] 
-  dat <- dat[!(dat$zipcode %in% c(missing_zipcodes, AK_zips)),]
-  dat2 <- dat %>% filter(control_indicator == 0 & lag_wk1 == 1) %>% mutate(pt = pt/7) %>% group_by(year) %>% summarise(pop_exposed = sum(pt))
-}
-
-pop <- ggplot(data=dat2, aes(x=year, y=pop_exposed, group = 1)) +
-  geom_line()+
-  geom_point()+
-  xlab("Year") +
-  ylab("Population impacted by flood exposure")
-
-figure_1 <- ggarrange(state_map_flood, pop, nrow=2,ncol=1,labels=c("A","B"),heights = c(6,4))
-
-  
+usa <- tm_shape(contig_us) + 
+  tm_polygons(col = "Freq", pal = colorpalette, style = "cont", border.col = "black", lwd = 0.1, title = "Number of unique flood exposures by ZIP Code", legend.is.portrait = FALSE) + 
+  tmap_options(check.and.fix = TRUE) + 
+  tm_layout(frame = FALSE, legend.outside=TRUE, legend.position = c("bottom", "center"))
+usa
